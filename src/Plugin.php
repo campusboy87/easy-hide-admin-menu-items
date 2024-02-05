41d9ec9a17ff20e2eea03a300e0eb8ec41daad10
Dan Zakirov <alexodiy@gmail.com>
2024-02-06 01:45:55 +0300

    add: Activation hook and Review class setup
    # Registered the activation hook in the Plugin class to call the 'plugin_activate' method when the plugin is activated. Additionally, instantiated a new Review class and called its 'init' method for initialization.
    
    # Activated hook callback function added to handle initial setup by checking and updating the 'ehami_data_install' option.

--- src/Plugin.php
+++ src/Plugin.php
@@ -33,10 +33,15 @@ class Plugin {
 		$this->path     = dirname( $main_file_path );
 		$this->url      = plugins_url( '', $main_file_path );
 		$this->basename = plugin_basename( $main_file_path );
-		$this->info     = get_file_data( $main_file_path, [
-			'name'    => 'Plugin Name',
-			'version' => 'Version',
-		] );
+		$this->info     = get_file_data(
+			$main_file_path,
+			array(
+				'name'    => 'Plugin Name',
+				'version' => 'Version',
+			)
+		);
+
+		register_activation_hook( $main_file_path, array( $this, 'plugin_activate' ) );
 	}
 
 	public function init() {
@@ -55,6 +60,28 @@ class Plugin {
 
 		$settings_page = new Settings_Page();
 		$settings_page->init();
+
+		$review = new Review();
+		$review->init();
 	}
 
+	/**
+	 * Activation hook callback function.
+	 */
+	public function plugin_activate() {
+
+		$existing_values = get_option( 'ehami_data_install', array() );
+
+		if ( is_array( $existing_values ) ) {
+			return;
+		}
+
+		$initial_values = array(
+			'date'   => gmdate( 'Y-m-d', strtotime( '+2 days' ) ),
+			'status' => 'activated',
+		);
+
+		// Update the option with initial values.
+		update_option( 'ehami_data_install', $initial_values );
+	}
 }
